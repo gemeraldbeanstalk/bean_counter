8ea8bbed31d7ef5053f346396291e56d18941a7f
Danny Guinther <dannyguinther@gmail.com>
2013-12-10 21:20:08 -0500

    Add docs to BeanCounter::SpecMatchers

--- lib/bean_counter/spec_matchers.rb
+++ lib/bean_counter/spec_matchers.rb
@@ -1,9 +1,23 @@
 module BeanCounter::SpecMatchers
 
+  # Creates a new BeanCounter::EnqueuedExpectation with +expected+ stored for
+  # later use when matching. Most of the time the value provided for +expected+
+  # will be ignored, the only exception is when +expected+ is given a block.
+  # When a block is provided for +expected+, only jobs enqueued during the
+  # execution of the block will be considered when matching.
+  #
+  # See BeanCounter::EnqueuedExpectation for additional information and usage
+  # patterns.
   def have_enqueued(expected)
     BeanCounter::EnqueuedExpectation.new(expected)
   end
 
+  # Creates a new BeanCounter::TubeExpectation with +expected+ stored for
+  # later use when matching. However, +expected+ is never used when matching.
+  # Instead, all tubes are matched against until a match is found.
+  #
+  # See BeanCounter::TubeExpectation for additional information and usage
+  # patterns.
   def have_tube(expected)
     BeanCounter::TubeExpectation.new(expected)
   end
